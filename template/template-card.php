d59d9c546b576f8f3c5a716c67f4e0e5ff4b4ec2
IamYuriko <nrtyrk@gmail.com>
2024-10-17 22:20:40 +0900

    再提出

--- template/template-card.php
+++ template/template-card.php
@@ -3,7 +3,11 @@
     <?php while (have_posts()) : the_post(); ?>
         <section class="p-card">
             <div class="p-card__contents c-grid">
-                <?php the_post_thumbnail('full', array('class' => 'p-card__image c-image')); ?>
+                <?php if (has_post_thumbnail()): ?>
+                    <?php the_post_thumbnail('full', array('class' => 'p-card__image c-image')); ?>
+                <?php else: ?>
+                    <img class="c-image" src="<?php echo get_template_directory_uri(); ?>/assets/images/pc-card_archive@2x.jpg" alt="no-image">
+                <?php endif; ?>
                 <div class="p-card__container">
                     <h2 class="p-card__title c-heading c-heading--xl"><?php the_title(); ?></h2>
                     <h3 class="p-card__subtitle c-heading c-heading--sm"><?php echo get_post_meta($post->ID, 'card_title', true); ?>
