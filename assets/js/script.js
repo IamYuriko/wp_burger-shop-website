35bed69a9a12276d5154e9d921812548865ae44c
IamYuriko <nrtyrk@gmail.com>
2024-10-17 19:13:04 +0900

    再提出

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ jQuery(document).ready(function($){
     $(".l-header__openBtn").click(function () { //ボタンがクリックされたら
         $(".l-sidebar").addClass("is-active"); //.l-sidebarに.is-activeを付与
         $(".p-overlay-hamburgerMenu").addClass("is-active"); //.l-wrapper__overlayに.is-activeを付与
-        $("body").css("overflow", "hidden"); //bodyにoverflow: hidden;を指定 //position:fixed;でもOK
+        $("body").css("position", "fixed"); //bodyにposition: fixed;を指定 
     });
 });
 
@@ -12,6 +12,6 @@ jQuery(document).ready(function($){
     $(".p-sideMenu__closeBtn").click(function () { //ボタンがクリックされたら
         $(".l-sidebar").removeClass("is-active"); //.l-sidebarの.is-activeを削除
         $(".p-overlay-hamburgerMenu").removeClass("is-active"); //.l-wrapper__overlayの.is-activeを削除
-        $("body").css("overflow", ""); //bodyからoverflow: hidden;を解除
+        $("body").css("position", "static"); //bodyからposition:fixed;を解除
     });
 });
