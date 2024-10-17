45d171ce7bff22e64faee04de805c22c45ecdb78
IamYuriko <nrtyrk@gmail.com>
2024-10-17 19:57:24 +0900

    再提出

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,4 @@
 //ハンバーガーメニューボタン開閉
-
 jQuery(document).ready(function($){
     $(".l-header__openBtn").click(function () { //ボタンがクリックされたら
         $(".l-sidebar").addClass("is-active"); //.l-sidebarに.is-activeを付与
@@ -12,6 +11,16 @@ jQuery(document).ready(function($){
     $(".p-sideMenu__closeBtn").click(function () { //ボタンがクリックされたら
         $(".l-sidebar").removeClass("is-active"); //.l-sidebarの.is-activeを削除
         $(".p-overlay-hamburgerMenu").removeClass("is-active"); //.l-wrapper__overlayの.is-activeを削除
-        $("body").css("position", "static"); //bodyからposition:fixed;を解除
+        $("body").css("position", "static"); //bodyのposition:fixed;を解除
     });
+});
+
+
+//画面サイズが切り替わる際の動作
+jQuery(document).ready(function($){
+  $(window).resize(function(){ //画面サイズが切り替わったとき
+    $("body").css("position", "static"); //bodyをposition:fixed;→ position:static;に変更
+    $(".l-sidebar").removeClass("is-active"); //.l-sidebarの.is-activeを削除、画面サイズが切り替わった際にサイドメニューが閉じる
+    $(".p-overlay-hamburgerMenu").removeClass("is-active"); //.l-wrapper__overlayの.is-activeを削除、画面サイズが切り替わった際にサイドメニューが閉じる
+  });
 });
