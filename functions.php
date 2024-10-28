2b768edfe969ca78763ab945e04b8f15d85a93c2
IamYuriko <nrtyrk@gmail.com>
2024-10-29 01:27:33 +0900

    デプロイ用バックアップ

--- functions.php
+++ functions.php
@@ -5,13 +5,10 @@ function my_stylesheets()
     //Google Fonts
     wp_enqueue_style('roboto', '//fonts.googleapis.com/css2?family=Roboto:wght@700&display=swap', array());
     wp_enqueue_style('mplus1p', '//fonts.googleapis.com/css2?family=M+PLUS+1p:wght@400;700&family=Roboto:wght@700&display=swap', array());
-
     //リセットCSS
     wp_enqueue_style('ress', '//unpkg.com/ress/dist/ress.min.css', array(), false, 'all');
-
     //メインのスタイルシート
     wp_enqueue_style('style', get_theme_file_uri('/assets/css/style.css'), array('ress'), false, 'all');
-
     //JavaScript
     wp_enqueue_script(
         'jquery',
--- page.php
+++ page.php
@@ -5,7 +5,7 @@
                                                                                 if (has_post_thumbnail()) :
                                                                                     echo esc_url(get_the_post_thumbnail_url()); // アイキャッチ画像のURLを取得
                                                                                 else:
-                                                                                    echo esc_url(get_template_directory_uri() . '/assets/images/pc-mv_single@2x.jpg'); // デフォルト画像のURLを指定
+                                                                                    echo esc_url(get_template_directory_uri() . '/assets/images/pc-mv_single@2x.jpg'); // no image画像のURLを指定
                                                                                 endif;
                                                                                 ?>');">>
             <h1 class="p-fv-page__title c-heading c-heading--xxxl"><?php the_title(); ?></h1>
